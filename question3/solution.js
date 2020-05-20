ae02002bcce313025a70e5589c199b8ec2ebf515
Ridza Adhandra <andradhandra@gmail.com>
2020-05-21 02:03:21 +0700

    Adding loops for question 3

--- question3/solution.js
+++ question3/solution.js
@@ -1,16 +1,39 @@
 function solution(relation) {
   let answer = 0;
-  if (relation.length > 20) return "Error: database tuple exceed limit"
+  if (relation.length > 20) return "Error: relation tuple exceed limit";
+  if (relation.filter((coloumn) => coloumn.length > 8).length > 0)
+    return "Error: relation attribute exceed limit";
 
-  let attributeLimit = false
-  
+  let col = 0
+  while (col < relation[0].length) {
+    let arr = []
+    relation.forEach(row => {
+      arr.push(row[col])
+    })
+    let unique = [ ...new Set(arr)]
+    if (arr.length === unique.length) answer += 1
+    col += 1
+  }
 
+  for (let i=0; i<relation[i].length; i++) {
+    for (let j=i+1; j<relation[i].length; j++) {
+      let arr = []
+      for (let row=0; row<relation.length; row++) {
+        // console.log([relation[row][i], relation[row][j]], 'test')
+          arr.push(relation[row][i] + " " + relation[row][j])
+      }
+      let unique = [...new Set(arr)]
+      console.log(arr, 'ini arr')
+      console.log(unique, 'ini unique')
+      if (arr.length === unique.length) answer += 1
+    }
+  }
 
   return answer;
 }
 
 // Input and output examples
-// relation: 
+// relation:
 // [
 //     [“100”,”ryan”,”music”,”2”],
 //     [“200”,”apeach”,”math”,”2”],
@@ -19,13 +42,15 @@ function solution(relation) {
 //     [“500”,”muzi”,”music”,”3”],
 //     [“600”,”apeach”,”music”,”2”]
 // ]
-console.log(solution([
-  ["100","ryan”,”music”,”2"],
-  ["200”,”apeach”,”math”,”2"],
-  ["300”,”tube”,”computer”,”3"],
-  ["400”,”con”,”computer”,”4"],
-  ["500”,”muzi”,”music”,”3"],
-  ["600”,”apeach”,”music”,”2"]
-]))
+console.log(
+  solution([
+    ["100", "ryan", "music", "2"],
+    ["200", "apeach","math","2"],
+    ["300", "tube","computer","3"],
+    ["400", "con","computer","4"],
+    ["500", "muzi","music","3"],
+    ["600", "apeach","music","2"],
+  ])
+);
 
 // answer: 2
