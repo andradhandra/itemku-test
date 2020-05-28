5e79be4e752632152e3d4a21ebf9ad64e163cd66
Ridza Adhandra <andradhandra@gmail.com>
2020-05-28 10:47:37 +0700

    alternative answer

--- question3/solution.js
+++ question3/solution.js
@@ -4,28 +4,59 @@ function solution(relation) {
   if (relation.filter((coloumn) => coloumn.length > 8).length > 0)
     return "Error: relation attribute exceed limit";
 
-  let col = 0
-  while (col < relation[0].length) {
-    let arr = []
-    relation.forEach(row => {
-      arr.push(row[col])
-    })
-    let unique = [ ...new Set(arr)]
-    if (arr.length === unique.length) answer += 1
-    col += 1
-  }
+  // let col = 0
+  // while (col < relation[0].length) {
+  //   let arr = []
+  //   relation.forEach(row => {
+  //     arr.push(row[col])
+  //   })
+  //   let unique = [ ...new Set(arr)]
+  //   if (arr.length === unique.length) answer += 1
+  //   col += 1
+  // }
 
-  for (let i=0; i<relation[i].length; i++) {
-    for (let j=i+1; j<relation[i].length; j++) {
+  // for (let i=0; i<relation[i].length; i++) {
+  //   for (let j=i+1; j<relation[i].length; j++) {
+  //     let arr = []
+  //     for (let row=0; row<relation.length; row++) {
+  //       // console.log([relation[row][i], relation[row][j]], 'test')
+  //         arr.push(relation[row][i] + " " + relation[row][j])
+  //     }
+  //     let unique = [...new Set(arr)]
+  //     console.log(arr, 'ini arr')
+  //     console.log(unique, 'ini unique')
+  //     if (arr.length === unique.length) answer += 1
+  //   }
+  // }
+
+  for (let i=1; i<=relation[0].length-1; i++) {
+    console.log(i, "ini jumlah kolom yg diperiksa====================================")
+    for (let j=0; j<relation[0].length; j++) {
       let arr = []
-      for (let row=0; row<relation.length; row++) {
-        // console.log([relation[row][i], relation[row][j]], 'test')
-          arr.push(relation[row][i] + " " + relation[row][j])
+      let arr2 = []
+      for (let k=0; k<relation.length; k++) {
+        if (i < 2) {
+          for (let l=0; l<i; l++) {
+            arr2.push(relation[k][j+l])
+          }
+        } else {
+          for (let l=0; l<=relation[0].length-i; l++) {
+            
+          }
+        }
+
+        if (arr2.length >= i) arr.push(arr2)
+        arr2 = []
       }
-      let unique = [...new Set(arr)]
-      console.log(arr, 'ini arr')
-      console.log(unique, 'ini unique')
-      if (arr.length === unique.length) answer += 1
+
+      if (arr.length) {
+        let unique = [...new Set(arr)]
+        if (arr.length === unique.length) {
+          answer += 1
+        }
+        console.log(arr, 'arr')
+        console.log(unique, 'unique')
+      }
     }
   }
 
