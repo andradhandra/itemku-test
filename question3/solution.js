e67f9970cbfa2cf14b9c1b7d47cda79ae4e98aa7
Ridza Adhandra <andradhandra@gmail.com>
2020-06-03 14:42:15 +0700

    final commit

--- question3/solution.js
+++ question3/solution.js
@@ -4,62 +4,31 @@ function solution(relation) {
   if (relation.filter((coloumn) => coloumn.length > 8).length > 0)
     return "Error: relation attribute exceed limit";
 
-  // let col = 0
-  // while (col < relation[0].length) {
-  //   let arr = []
-  //   relation.forEach(row => {
-  //     arr.push(row[col])
-  //   })
-  //   let unique = [ ...new Set(arr)]
-  //   if (arr.length === unique.length) answer += 1
-  //   col += 1
-  // }
+  for (let i = 0; i < relation[0].length; i++) {
+    let arr = [];
 
-  // for (let i=0; i<relation[i].length; i++) {
-  //   for (let j=i+1; j<relation[i].length; j++) {
-  //     let arr = []
-  //     for (let row=0; row<relation.length; row++) {
-  //       // console.log([relation[row][i], relation[row][j]], 'test')
-  //         arr.push(relation[row][i] + " " + relation[row][j])
-  //     }
-  //     let unique = [...new Set(arr)]
-  //     console.log(arr, 'ini arr')
-  //     console.log(unique, 'ini unique')
-  //     if (arr.length === unique.length) answer += 1
-  //   }
-  // }
+    for (let j = 0; j < relation.length; j++) {
+      let str = "";
+      str += relation[j][i];
+      arr.push(str);
+    }
 
-  for (let i=1; i<=relation[0].length-1; i++) {
-    console.log(i, "ini jumlah kolom yg diperiksa====================================")
-    for (let j=0; j<relation[0].length; j++) {
-      let arr = []
-      let arr2 = []
-      for (let k=0; k<relation.length; k++) {
-        if (i < 2) {
-          for (let l=0; l<i; l++) {
-            arr2.push(relation[k][j+l])
-          }
-        } else {
-          for (let l=0; l<=relation[0].length-i; l++) {
-            
-          }
-        }
-
-        if (arr2.length >= i) arr.push(arr2)
-        arr2 = []
+    let arr2 = [...arr];
+    for (let k = i; k < relation[0].length; k++) {
+      arr2 = [...arr];
+      for (let j = 0; j < arr2.length; j++) {
+        if (!relation[j][k] || k === i) break;
+        arr2[j] += relation[j][k];
       }
 
-      if (arr.length) {
-        let unique = [...new Set(arr)]
-        if (arr.length === unique.length) {
-          answer += 1
-        }
-        console.log(arr, 'arr')
-        console.log(unique, 'unique')
+      let unique = [...new Set(arr2)];
+      if (arr.length === unique.length) {
+        console.log(unique, "unique");
+        answer += 1;
+        break;
       }
     }
   }
-
   return answer;
 }
 
@@ -76,11 +45,11 @@ function solution(relation) {
 console.log(
   solution([
     ["100", "ryan", "music", "2"],
-    ["200", "apeach","math","2"],
-    ["300", "tube","computer","3"],
-    ["400", "con","computer","4"],
-    ["500", "muzi","music","3"],
-    ["600", "apeach","music","2"],
+    ["200", "apeach", "math", "2"],
+    ["300", "tube", "computer", "3"],
+    ["400", "con", "computer", "4"],
+    ["500", "muzi", "music", "3"],
+    ["600", "apeach", "music", "2"],
   ])
 );
 
